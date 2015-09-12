c20de5eb08f3d44522d811bdb16479ed66728ce6
andreajeka <andreajeka@live.com>
2015-09-12 19:05:34 +0800

    Add basic functionality for circle brush - Might need a better algorithm to make circle smoother - Must think about performance or display`

---
+++ CircleBrush.cpp
@@ -0,0 +1,55 @@
+//
+// CircleBrush.cpp
+//
+// The implementation of CircleBrush Brush. It is a kind of ImpBrush. All your brush implementations
+// will look like the file with the different GL primitive calls.
+//
+
+#include "impressionistDoc.h"
+#include "impressionistUI.h"
+#include "CircleBrush.h"
+#include "math.h"
+
+extern float frand();
+
+CircleBrush::CircleBrush(ImpressionistDoc* pDoc, char* name) :
+ImpBrush(pDoc, name)
+{
+}
+
+void CircleBrush::BrushBegin(const Point source, const Point target)
+{
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg = pDoc->m_pUI;
+
+	BrushMove(source, target);
+}
+
+void CircleBrush::BrushMove(const Point source, const Point target)
+{
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg = pDoc->m_pUI;
+
+	if (pDoc == NULL) {
+		printf("CircleBrush::BrushMove  document is NULL\n");
+		return;
+	}
+
+	int brushSize = pDoc->getSize();
+	int radius = brushSize / 2;
+	glBegin(GL_TRIANGLE_FAN);
+	SetColor(source);
+
+	for (int i = 0; i < 360; i++)
+	{
+		double degInRad = i * M_PI / 180.0;
+		glVertex2d(cos(degInRad)*radius + target.x, sin(degInRad)*radius + target.y);
+	}
+
+	glEnd();
+}
+
+void CircleBrush::BrushEnd(const Point source, const Point target)
+{
+	// do nothing so far
+}
