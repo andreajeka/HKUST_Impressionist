dd1de6fccefedf6dd9770388e13095b31628dc14
andreajeka <andreajeka@live.com>
2015-09-12 19:00:29 +0800

    Add basic functionality for point & line brush - Point brush was already implemented in skeleton file

---
+++ LineBrush.cpp
@@ -0,0 +1,54 @@
+//
+// LineBrush.cpp
+//
+// The implementation of Line Brush. It is a kind of ImpBrush. All your brush implementations
+// will look like the file with the different GL primitive calls.
+//
+
+#include "impressionistDoc.h"
+#include "impressionistUI.h"
+#include "LineBrush.h"
+
+extern float frand();
+
+LineBrush::LineBrush(ImpressionistDoc* pDoc, char* name) :
+ImpBrush(pDoc, name)
+{
+}
+
+void LineBrush::BrushBegin(const Point source, const Point target)
+{
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg = pDoc->m_pUI;
+
+	BrushMove(source, target);
+}
+
+void LineBrush::BrushMove(const Point source, const Point target)
+{
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg = pDoc->m_pUI;
+
+	if (pDoc == NULL) {
+		printf("PointBrush::BrushMove  document is NULL\n");
+		return;
+	}
+
+	int size = pDoc->getSize();
+
+	glBegin(GL_LINES);
+		SetColor(source);
+
+		// Because a line is made of two points,
+		// we have to supply the location of two points.
+		glVertex2d(target.x, target.y);
+		glVertex2d(target.x + size, target.y);
+
+	glEnd();
+}
+
+void LineBrush::BrushEnd(const Point source, const Point target)
+{
+	// do nothing so far
+}
+
